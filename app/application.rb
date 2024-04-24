79ca94f189661f13f631bdca4cfbb0ae5ba8d9c9
mariana castro <mariananycastro@gmail.com>
2024-04-24 20:58:59 -0300

    add EXPIRATE_AFTER to application

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,7 @@ require_relative './requesters/policies_by_email_requester'
 require_relative './requesters/create_policy_requester'
 
 class Application < Sinatra::Base
+  EXPIRATE_AFTER = 1200 # seconds
   LOGIN_PATHS = %r{/(login|logout)}
   AUTH_CALLBACK_PATHS = %r{/(auth/[^/]+/(callback|failure))}
 
