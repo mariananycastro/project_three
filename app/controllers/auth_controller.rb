e17d801e9c1b18d0a23e1860f64c01a991d0b0b6
mariana castro <mariananycastro@gmail.com>
2024-04-26 16:48:37 -0300

    fix refactor controllers

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,45 @@
+require 'omniauth'
+require 'omniauth-google-oauth2'
+require_relative '../../db/database'
+require_relative 'base_controller'
+
+class AuthController < BaseController
+  use OmniAuth::Builder do
+    provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], scope: 'email'
+  end
+
+  get '/login' do
+    erb :login, layout: :application,
+    locals: {
+      google_key: ENV['GOOGLE_KEY'],
+      csrf_token: request.env['rack.session']['csrf']
+    }
+  end
+
+  post '/logout' do
+    Session.expire_all(omniauth_auth_email)
+    redirect '/login'
+  end
+
+  get '/auth/:provider/callback' do
+    content_type 'text/plain'
+
+    session[:omniauth_auth] = request.env['omniauth.auth'].to_hash
+    user_email = session[:omniauth_auth]['info']['email']
+
+    Session.expire_all(user_email)
+
+    Session.create(
+      session_id: session[:session_id],
+      email: user_email,
+      expires_at: Application::EXPIRES_SESSION.seconds.from_now
+    )
+
+    redirect '/'
+  end
+
+  get '/auth/:provider/failure' do
+    content_type 'text/plain'
+    'Failure -> callback error'
+  end
+end
---
+++ app/helpers/session_helper.rb
@@ -0,0 +1,14 @@
+module SessionHelper
+  def user_signed_in?
+    if omniauth_auth_email
+      current_session = Session.active_session(omniauth_auth_email)
+      return true if current_session && current_session.session_id_correct?(session[:session_id])
+    end
+
+    false
+  end
+
+  def omniauth_auth_email
+    session.dig(:omniauth_auth, 'info', 'email')
+  end
+end
